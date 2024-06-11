6ecbd05cf0b8bb17c522d80c1892c67759537a24
Sultan Qasim Khan <sultanqasim@yandex.com>
2024-06-11 18:10:32 -0400

    Update capture script for more data

--- rfnm_test.py
+++ rfnm_test.py
@@ -2,22 +2,24 @@
 
 import SoapySDR
 import numpy
+import sys
 
-def main():
+def main(samp_rate_index):
     print("Opening RFNM")
     args = dict(driver="RFNM")
     sdr = SoapySDR.Device(args)
 
     print("Configuring")
     rates = sdr.listSampleRates(SoapySDR.SOAPY_SDR_RX, 0)
-    print("Setting sample rate", rates[1])
-    sdr.setSampleRate(SoapySDR.SOAPY_SDR_RX, 0, rates[0])
+    print("Setting sample rate", rates[samp_rate_index])
+    sdr.setSampleRate(SoapySDR.SOAPY_SDR_RX, 0, rates[samp_rate_index])
 
     antennas = sdr.listAntennas(SoapySDR.SOAPY_SDR_RX, 0)
     print("Setting antenna", antennas[0])
     sdr.setAntenna(SoapySDR.SOAPY_SDR_RX, 0, antennas[0])
 
-    sdr.setFrequency(SoapySDR.SOAPY_SDR_RX, 0, 2.7E9)
+    sdr.setBandwidth(SoapySDR.SOAPY_SDR_RX, 0, 80E6)
+    sdr.setFrequency(SoapySDR.SOAPY_SDR_RX, 0, 2.1E9)
     sdr.setGain(SoapySDR.SOAPY_SDR_RX, 0, -23)
 
     print("Setting up stream")
@@ -25,7 +27,7 @@ def main():
     sdr.activateStream(rxStream)
 
     print("Fetching samples")
-    CHUNK_SZ = 1 << 18
+    CHUNK_SZ = 1 << 20
     buff = numpy.array([0]*CHUNK_SZ, numpy.complex64)
     for i in range(100):
         sr = sdr.readStream(rxStream, [buff], CHUNK_SZ)
@@ -36,11 +38,11 @@ def main():
             print("Got chunk %d" % i)
 
     print("Writing chunk to file")
-    buff.tofile("samples_rfnm_%d.cf32" % rates[1])
+    buff.tofile("samples_rfnm_%d.cf32" % rates[samp_rate_index])
 
     print("Closing")
     sdr.deactivateStream(rxStream)
     sdr.closeStream(rxStream)
 
 if __name__ == "__main__":
-    main()
+    main(int(sys.argv[1]))
