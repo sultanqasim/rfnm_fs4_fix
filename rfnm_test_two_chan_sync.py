6628c6fa3646806d069ef04316ddd63eac94e927
Sultan Qasim Khan <sultanqasim@yandex.com>
2024-07-22 14:21:41 -0400

    Channel sync test improvements

--- rfnm_test_two_chan_sync.py
+++ rfnm_test_two_chan_sync.py
@@ -4,9 +4,24 @@ import SoapySDR
 import numpy
 from time import time
 import matplotlib.pyplot as plt
+from scipy import fftpack
 
 NUM_CHANNELS = 2
 CHUNK_SZ = 1 << 18
+
+# https://stackoverflow.com/questions/4688715/find-time-shift-between-two-similar-waveforms
+def find_time_shift(a, b):
+    A = fftpack.fft(a)
+    B = fftpack.fft(b)
+    Ar = -A.conjugate()
+    Br = -B.conjugate()
+    shiftA = numpy.argmax(numpy.abs(fftpack.ifft(Ar*B)))
+    shiftB = numpy.argmax(numpy.abs(fftpack.ifft(A*Br)))
+
+    if shiftA < shiftB:
+        return shiftA
+    else:
+        return -shiftB
 
 def main():
     print("Opening RFNM")
@@ -29,7 +44,7 @@ def main():
     # allocate buffers and files
     buffs = []
     captures = []
-    samples_to_read = CHUNK_SZ * 50
+    samples_to_read = CHUNK_SZ * 20
     for i in range(NUM_CHANNELS):
         buffs.append(numpy.zeros(CHUNK_SZ, numpy.complex64))
         captures.append(numpy.zeros(samples_to_read, numpy.complex64))
@@ -42,7 +57,7 @@ def main():
     print("Fetching samples")
     samples_read = 0
     while samples_read < samples_to_read:
-        sr = sdr.readStream(rxStream, buffs, CHUNK_SZ, timeoutUs=0)
+        sr = sdr.readStream(rxStream, buffs, CHUNK_SZ)
         for i in range(NUM_CHANNELS):
             captures[i][samples_read:samples_read + sr.ret] = buffs[i][:sr.ret]
             buffs[i][:] = 0
@@ -50,17 +65,16 @@ def main():
         print("Read %d samples, time %.3f" % (samples_read, sr.timeNs / 1e9), end='\r')
     t_end = time()
 
-    samp_rate = samples_to_read / (t_end - t_start)
-    print("\nSample rate: %.3f Msps" % (samp_rate / 1E6))
-
     print("Closing stream")
     sdr.deactivateStream(rxStream)
     sdr.closeStream(rxStream)
 
+    print("Channel 1 delay:", -find_time_shift(captures[0], captures[1]), "samples")
+
     print("Plotting")
     fig, axs = plt.subplots(2)
     fig.set_dpi(240)
-    fig.set_size_inches(6, 8)
+    #fig.set_size_inches(6, 8)
     axs[0].plot(numpy.abs(captures[0]))
     axs[1].plot(numpy.abs(captures[1]))
     fig.tight_layout()
